4a47da001ace9ce84e73048935ec8c780840a418
Pedro Neto <19939247+pedropamn@users.noreply.github.com>
2024-11-16 21:40:58 -0300

    Fix for infinite loop without apparent reason

--- Hide WhatsApp Web Conversations.user.js
+++ Hide WhatsApp Web Conversations.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide WhatsApp Web Conversations
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Hide WhatsApp conversations panel on WhatsApp Web
 // @author       Pedro Neto
 // @match        https://web.whatsapp.com/
@@ -22,7 +22,7 @@
         var panel = $('#side').is(":visible");
 
         //When visible, insert the icon
-        if(panel){
+        if(panel && $('.hide_conversations').length === 0){
             var html = '<div class="hide_conversations" style="cursor:pointer;"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet">' +
     '<g transform="translate(0,512) scale(0.1,-0.1)" fill="#000000" stroke="none">' +
     '<path d="M4260 4470 c-14 -6 -147 -133 -297 -282 l-271 -271 -129 50 c-208 83 -406 135 -638 170 -184 27 -571 24 -764 -6 -509 -78 -959 -281 -1385 -624 -135 -108 -391 -366 -500 -502 -118 -149 -264 -368 -272 -410 -11 -60 4 -100 86 -223 240 -363 584 -709 930 -937 58 -38 107 -70 109 -72 3 -2 -104 -113 -236 -246 -207 -207 -242 -247 -248 -280 -11 -60 3 -107 44 -148 41 -41 88 -55 148 -44 34 6 227 196 1831 1798 1317 1317 1795 1801 1803 1826 22 67 -2 139 -63 184 -31 23 -111 32 -148 17z m-1360 -655 c172 -26 363 -76 514 -135 l30 -11 -164 -164 -164 -164 -27 19 c-52 38 -215 110 -294 132 -106 28 -344 31 -449 5 -183 -46 -328 -128 -462 -261 -133 -134 -215 -280 -261 -462 -27 -108 -24 -334 5 -444 23 -83 89 -234 131 -295 l20 -31 -203 -203 -203 -203 -95 59 c-332 209 -669 527 -878 829 l-51 74 50 73 c177 256 485 564 746 748 352 248 768 405 1181 448 124 13 448 5 574 -14z m-198 -634 c62 -16 178 -64 178 -74 0 -1 -195 -197 -434 -436 l-434 -434 -27 52 c-36 73 -65 191 -65 270 1 409 394 721 782 622z"></path>' +
